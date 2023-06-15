d7f234b7d10bef0225111b65a1092a1c89190900
darshanmisaal1 <136681173+darshanmisaal1@users.noreply.github.com>
2023-06-15 12:05:46 +0530

    Hello World

---
+++ helloworld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10; // The compiler version should be equal to or greater than 0.8.10
+
+contract HelloWorld {
+    
+    /*
+      Syntax:
+      <dataType> <visibility> <varName> = <data>;
+    */
+    
+    string public myString = "Hello World, Darshan Here"; // Declares a public string variable named 'myString' with the initial value "Hello World, Aman Here"
+}
